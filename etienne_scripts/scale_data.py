0a19b8b76b4efefa6613e8a0b98a32be09fac9e4
Etienne92 <etienne.sollier@polytechnique.edu>
2019-04-23 10:51:50 +0100

    Use highly_variable_genes instead of filter_genes_dispersion

--- etienne_scripts/scale_data.py
+++ etienne_scripts/scale_data.py
@@ -7,5 +7,6 @@ if len(sys.argv)<3:
 infile = sys.argv[1]
 outfile = sys.argv[2]
 adata = sc.read(infile)
-adata.X=np.log1p(adata.X)
+#Don't do anything, as the logarithmization is now done in the previous step.
+#adata.X=np.log1p(adata.X)
 adata.write(outfile)
